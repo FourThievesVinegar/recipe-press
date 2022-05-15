8cf97ad25e3fd09b16ac561963b55b4801b23b11
Mike Kissinger <mis4mike@gmail.com>
2022-05-15 17:09:43 -0400

    parameter listing with inputs and some formatting

--- src/components/StepDetails.js
+++ src/components/StepDetails.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { useRecipeContext } from '../contexts/RecipeContext'
 import { NewStep } from './NewStep'
 import { RecipeStep } from './RecipeStep'
-
 import './StepDetails.css'
 
 export const StepDetails = () => {
